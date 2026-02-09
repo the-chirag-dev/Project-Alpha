f4f9c2c3e63c30f9f25daf7e2a084df528403b07
the-chirag-dev <chiragbhomare@gmail.com>
2026-02-09 14:44:14 +0530

    feat: setup python validator and html skeleton

---
+++ logic_drills.py
@@ -0,0 +1,8 @@
+# Task: Swap the variables without using a third 'temp' variable.
+a= 10
+b= 20
+print(f"BEfore a={a} and b={b}")
+
+#the logic (pythonic way)
+a,b = b,a
+print(f"After a={a} and b={b}")
