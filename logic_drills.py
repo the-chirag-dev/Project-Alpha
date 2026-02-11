5a99e4c35c855fe0169b10f7505b45ce5d4b5d7d
the-chirag-dev <chiragbhomare@gmail.com>
2026-02-11 11:55:10 +0530

    logic: catch-up complete, added nav links and if-else refactor

--- logic_drills.py
+++ logic_drills.py
@@ -18,3 +18,22 @@ print(f"Remaining days until deadline: {remaining.days}")
 
 weeks_left = remaining.days // 7
 print(f"Weeks left until deadline: {weeks_left}")
+
+n=int(input("Enter an integer: "))
+if n % 2 != 0:
+    print("Weird")
+elif n % 2 == 0 and 2 <= n <= 5:
+    print("Not Weird")
+elif n % 2 == 0 and 6 <= n <= 20:
+    print("Weird")
+else: # This covers everything > 20
+    print("Not Weird")
+
+print(f"Calculation Complete.\nInput was: {n}")
+
+
+"""Task: Write a script that takes an integer n.
+If n is odd, print Weird.
+If n is even and in the inclusive range of 2 to 5, print Not Weird.
+If n is even and in the inclusive range of 6 to 20, print Weird.
+If n is even and greater than 20, print Not Weird."""
